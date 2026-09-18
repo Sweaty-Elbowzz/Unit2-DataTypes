29bbdbfae48eecb58386ec4297656502a0e156fe
Sweaty-Elbowzz <kingsleyn3@nycstudents.net>
2026-09-18 09:20:15 -0400

    Create string.py

---
+++ string.py
@@ -0,0 +1,21 @@
+#string for characters
+name = "Kingsley"
+print(name.upper())
+#input asks the usera question and records the answer
+#what we write in input argument is what the user sees
+#input ALWAYS outputs the string
+# bill = float(input("How much was the bill? "))
+# print(bill)
+
+# """ if bill == 10:
+#     print ("Match")
+# else:
+#     print ("No Match")
+# # integer for whole numbers
+# # amt = 100
+# # #Float uses decimals
+# # amt_2 = 99.99
+
+# #Boolean
+# x = True
+# y = False """
