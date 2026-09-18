c79e8a08078b331e12c8d9d288af561e55cdff7d
Sweaty-Elbowzz <kingsleyn3@nycstudents.net>
2026-09-18 09:20:19 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,37 @@
+# x = 3
+# y = float(3)
+# print(x,y)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+#     print(values[0])
+# print(values[6])
+
+# day_of_week = input("what day is it? ")
+# if day_of_week == "Friday":
+#     print("correct")
+# else:
+#     print("incorrect")
+
+# temp = 75
+# if temp > 68:
+#     print('warm')
+# elif temp == 68:
+#     print('perfect')
+# else:
+#     print('cold')
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+# question = input("Is 13 odd or even?")
+# print(question)
+# if question == "even":
+#     print("incorrect")
+# else:
+#     print("correct")
+
